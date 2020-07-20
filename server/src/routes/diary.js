7e7d238fd9ca83577be17eb5aabdad85db030203
rita-mordio <ritamordio0716@gmail.com>
2020-07-20 22:06:03 +0900

    다시 시작!!!!!

--- server/src/routes/diary.js
+++ server/src/routes/diary.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import Diary from "../schemas/Diary";
+import User from "../schemas/User";
 
 const router = express.Router();
 
@@ -21,6 +22,54 @@ router.post("/editDiary", (request, response) => {
   Diary.update({ _id: request.body.diaryId }, { $set: request.body })
     .then((result) => {
       response.status(200).json({ success: true, result });
+    })
+    .catch((error) => {
+      response.status(400).json({ success: false, error });
+    });
+});
+
+//내가 작성한 일기List 가져오기
+router.post("/getDetailMyDiaries", (request, response) => {
+  User.findOne({ token: request.body.token })
+    .then((user) => {
+      // Diary.find({ userFrom: user._id }).populate({ path: 'userFrom' })
+      Diary.find({ userFrom: user._id })
+        .then((diaries) => {
+          response.status(200).json({ success: true, diaries });
+        })
+        .catch((error) => {
+          response.status(400).json({ success: false, error });
+        });
+    })
+    .catch((error) => {
+      response.status(400).json({ success: false, error });
+    });
+});
+
+//단일 일기 객체 가져오기
+//일기 ID로 검색
+router.post("/getDiary", (request, response) => {
+  Diary.findOne({ _id: request.body.id })
+    .then((result) => {
+      response.status(200).json({ success: true, result });
+    })
+    .catch((error) => {
+      response.status(400).json({ success: false, error });
+    });
+});
+
+
+//제목으로 내가 작성한 일기 검색
+router.post("/searchMyDiariesByTitle", (request, response) => {
+  User.findOne({ token: request.body.token })
+    .then((user) => {
+      Diary.find({ userFrom: user._id, title: {'$regex': request.body.title}})
+        .then((result) => {
+          response.status(200).json({ success: true, result });
+        })
+        .catch((error) => {
+          response.status(400).json({ success: false, error });
+        });
     })
     .catch((error) => {
       response.status(400).json({ success: false, error });
