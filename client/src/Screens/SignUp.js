800e52f5a1b5fbadb1c973e579ede5acefcccec7
rita-mordio <ritamordio0716@gmail.com>
2020-07-21 20:25:25 +0900

    no message

--- client/src/Screens/SignUp.js
+++ client/src/Screens/SignUp.js
@@ -199,7 +199,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     paddingHorizontal: 20,
-    paddingVertical: 10,
+    paddingVertical: 20,
   },
   buttonStyle: {
     backgroundColor: '#efc4cd',
