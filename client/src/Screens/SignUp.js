c6ba33231303f47cf6b502aa917cff4d3f0b045c
KIRA <ykkee7@clinomiucs.co.kr>
2020-07-21 16:52:07 +0900

    회원가입 작업중

--- client/src/Screens/SignUp.js
+++ client/src/Screens/SignUp.js
@@ -1,6 +1,4 @@
 import {
-  Text,
-  View,
   TouchableOpacity,
   Keyboard,
   TextInput,
@@ -8,13 +6,55 @@ import {
   Platform,
   TouchableWithoutFeedback,
 } from 'react-native';
-import * as React from 'react';
+import React from 'react';
+import styled from 'styled-components/native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import FeatherIcon from 'react-native-vector-icons/Feather';
-import LinearGradient from 'react-native-linear-gradient';
+import {Button} from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 
-const SignUp = ({ navigation }) => {
+// const Container = styled.View`
+//   flex: 1;
+//   background-color: #efc4cd;
+// `;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #efc4cd;
+`;
+
+const TopView = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  padding-left: 20px;
+  padding-bottom: 50px;
+`;
+
+const TopTitle = styled.Text`
+  color: #2b2b2b;
+  font-family: Nanum Pen;
+  font-size: 50px;
+`;
+
+const BottomTitle = styled.Text`
+  color: #2b2b2b;
+  font-size: 18px;
+  margin-top: 20px;
+`;
+
+const InputWrap = styled.View`
+  flex-direction: row;
+  margin-top: 10px;
+  padding-bottom: 5px;
+  border-bottom-width: 1px;
+  border-color: #f2f2f2;
+`;
+
+const ButtonWrap = styled.View`
+  margin-top: 20px;
+`;
+
+const SignUp = ({navigation}) => {
   const [data, setData] = React.useState({
     id: '',
     password: '',
@@ -70,17 +110,16 @@ const SignUp = ({ navigation }) => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        {/*<StatusBar backgroundColor="#9DC8C8" barStyle="dark-content" />*/}
-        <View style={styles.header}>
-          <Text style={styles.text_header}>회원가입</Text>
-        </View>
-        <Animatable.View style={styles.footer} animation="fadeInUpBig">
-          <Text style={styles.text_footer}>ID</Text>
-          <View style={styles.action}>
+      <Container>
+        <TopView>
+          <TopTitle>회원가입</TopTitle>
+        </TopView>
+        <Animatable.View style={styles.bottom} animation="fadeInUpBig">
+          <BottomTitle>이메일</BottomTitle>
+          <InputWrap>
             <FontAwesomeIcon name="user-o" color="#05375a" size={20} />
             <TextInput
-              placeholder="당신의 소중한 아이디"
+              placeholder="당신의 소중한 이메일"
               style={styles.textInput}
               autoCapitalize="none"
               onChangeText={(val) => handleIdChange(val)}
@@ -90,10 +129,9 @@ const SignUp = ({ navigation }) => {
                 <FeatherIcon name="check-circle" color="green" size={20} />
               </Animatable.View>
             ) : null}
-          </View>
-          {/*<Text>이미 사용중인 아이디</Text>*/}
-          <Text style={[styles.text_footer, { marginTop: 20 }]}>Password</Text>
-          <View style={styles.action}>
+          </InputWrap>
+          <BottomTitle>비밀번호</BottomTitle>
+          <InputWrap>
             <FontAwesomeIcon name="lock" color="#05375a" size={20} />
             <TextInput
               placeholder="당신의 비밀스런 비밀번호"
@@ -109,10 +147,10 @@ const SignUp = ({ navigation }) => {
                 <FeatherIcon name="eye" color="gray" size={20} />
               )}
             </TouchableOpacity>
-          </View>
+          </InputWrap>
 
-          <Text style={[styles.text_footer, { marginTop: 20 }]}>Confirm Password</Text>
-          <View style={styles.action}>
+          <BottomTitle>비밀번호 확인</BottomTitle>
+          <InputWrap>
             <FontAwesomeIcon name="lock" color="#05375a" size={20} />
             <TextInput
               placeholder="중요한 건 한번 더 체크"
@@ -128,33 +166,17 @@ const SignUp = ({ navigation }) => {
                 <FeatherIcon name="eye" color="gray" size={20} />
               )}
             </TouchableOpacity>
-          </View>
+          </InputWrap>
 
-          <View style={styles.button}>
-            <TouchableOpacity style={styles.signIn}>
-              <LinearGradient colors={['#9dc8c8', '#58c9b9']} style={styles.signIn}>
-                <Text style={[styles.textSign, { color: '#2b2b2b' }]}>회원가입</Text>
-              </LinearGradient>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => {
-                navigation.goBack();
-              }}
-              style={[
-                styles.signIn,
-                {
-                  borderColor: '#9DC8C8',
-                  borderWidth: 1,
-                  marginTop: 15,
-                },
-              ]}
-            >
-              <Text style={[styles.textSign, { color: '#2b2b2b' }]}>로그인하러 갈래요</Text>
-            </TouchableOpacity>
-          </View>
+          <ButtonWrap>
+            <Button
+              buttonStyle={styles.buttonStyle}
+              title="회원가입"
+              raised={true}
+            />
+          </ButtonWrap>
         </Animatable.View>
-      </View>
+      </Container>
     </TouchableWithoutFeedback>
   );
 };
@@ -162,67 +184,21 @@ const SignUp = ({ navigation }) => {
 export default SignUp;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#9DC8C8',
-  },
-  header: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    paddingHorizontal: 20,
-    paddingBottom: 50,
-  },
-  footer: {
+  bottom: {
     flex: Platform.OS === 'ios' ? 3 : 5,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     paddingHorizontal: 20,
-    paddingVertical: 30,
-  },
-  text_header: {
-    color: '#2b2b2b',
-    fontFamily: 'Nanum Pen',
-    fontSize: 50,
-  },
-  text_footer: {
-    color: '#05375a',
-    fontSize: 18,
-  },
-  action: {
-    flexDirection: 'row',
-    marginTop: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#f2f2f2',
-    paddingBottom: 5,
+    paddingVertical: 10,
   },
   textInput: {
     flex: 1,
     marginTop: Platform.OS === 'ios' ? 0 : -12,
     paddingLeft: 10,
-    color: '#05375a',
+    color: '#2b2b2b',
   },
-  button: {
-    alignItems: 'center',
-    marginTop: 50,
-  },
-  signIn: {
-    width: '100%',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-  },
-  textSign: {
-    fontSize: 18,
-    fontFamily: 'NotoSerifKR-Medium',
-  },
-  textPrivate: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 20,
-  },
-  color_textPrivate: {
-    color: 'grey',
+  buttonStyle: {
+    backgroundColor: '#efc4cd',
   },
 });
