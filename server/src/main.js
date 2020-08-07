dedcc41bd71e6137d8a0de15a82d5eb1b3a757c0
rita-mordio <ritamordio0716@gmail.com>
2020-08-07 19:14:07 +0900

    서버 유지 관리 코드 추가

--- server/src/main.js
+++ server/src/main.js
@@ -1,26 +1,34 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors'
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
 
-import connect from './schemas'
-import userRouter from './routes/user';
-import diaryRouter from  './routes/diary'
+import connect from "./schemas";
+import userRouter from "./routes/user";
+import diaryRouter from "./routes/diary";
 
 const app = express();
 
 dotenv.config();
-connect()
+connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.use('/api/user', userRouter);
-app.use('/api/diary', diaryRouter);
+app.use("/api/user", userRouter);
+app.use("/api/diary", diaryRouter);
 
-app.listen(process.env.PORT, () =>
-  console.log(`서버 가동중 http://localhost:${process.env.PORT}`),
-);
+app.listen(process.env.PORT, () => {
+  console.log(`서버 가동중 http://localhost:${process.env.PORT}`);
+  process.send("ready");
+});
+
+process.on("SIGINT", () => {
+  app.close(() => {
+    console.log("server closed");
+    process.exit(0);
+  });
+});
